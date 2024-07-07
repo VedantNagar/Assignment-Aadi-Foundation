39894aa16a3b957514e33f988df55c65050eac5c
Vedant <vedantnagar21@gmail.com>
2024-07-07 21:27:39 +0530

    feat: Add Pages component for selecting a page

---
+++ frontend/src/components/Pages.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Pages = ({ accessToken, onPageSelect }) => {
+  const [pages, setPages] = useState([]);
+
+  useEffect(() => {
+    const fetchPages = async () => {
+      const response = await axios.post("http://localhost:5000/api/pages", {
+        accessToken,
+      });
+      setPages(response.data.data);
+    };
+    fetchPages();
+  }, [accessToken]);
+
+  return (
+    <div>
+      <select onChange={(e) => onPageSelect(e.target.value)}>
+        <option value="">Select a page</option>
+        {pages.map((page) => (
+          <option key={page.id} value={page.id}>
+            {page.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Pages;
