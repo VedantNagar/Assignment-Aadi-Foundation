ae4a18d69fce4c6323b50b1fa748457e89f95f30
Vedant <vedantnagar21@gmail.com>
2024-07-08 20:16:00 +0530

    chore: Update backend server to use async/await syntax for starting the server

--- backend/index.js
+++ backend/index.js
@@ -31,8 +31,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 5000;
-
 app.post("/api/auth/facebook", async (req, res) => {
   const { accessToken } = req.body;
   try {
@@ -87,6 +85,17 @@ app.post("/api/insights", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    console.log("connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
