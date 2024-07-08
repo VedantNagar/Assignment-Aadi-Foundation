a4fe21cae329a0c63810aaf60fb8fa66d01dcc9c
Vedant <vedantnagar21@gmail.com>
2024-07-08 17:49:44 +0530

    chore: Add HTTPS support for localhost in allowedOrigins

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 const allowedOrigins = [
   "http://localhost:5173",
+  "https://localhost:5173",
   "http://localhost:5174",
   "https://localhost:5174",
   "http://localhost:8000",
